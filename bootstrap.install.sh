08d06318344cbc35f096c5f1ea0279a860bbb741
root <root@tester.novalocal>
2019-10-16 10:33:31 +0200

    general: move repo to gitlab.meta

--- bootstrap.install.sh
+++ bootstrap.install.sh
@@ -4,10 +4,10 @@ apt-get update
 apt-get install -y git puppet
 
 if [ ! -d /puppet ]; then
-	git clone https://rsyslog.metacentrum.cz/rsyslog4.git /opt/rsyslog4
+	git clone https://gitlab.meta.zcu.cz/bodik/rsyslog4.git /opt/rsyslog4
 	ln -sf /opt/rsyslog4 /puppet
 	cd /puppet
-	git remote set-url origin --push bodik@rsyslog.metacentrum.cz:/data/rsyslog4.git
+	git remote set-url origin --push git@gitlab.meta.zcu.cz:bodik/rsyslog4.git
 else
 	cd /puppet
 	git pull
